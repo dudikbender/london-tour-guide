05abdfe21af34b4c40ece53103a7fed57e405200
David Bender <bender2242@gmail.com>
2022-01-20 00:40:46 +0000

    fixed typo

--- models/map.py
+++ models/map.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 from .load_data import retrieve_data
 
 load_dotenv()
+mapbox_token = environ.get('MAPBOX_TOKEN')
 px.set_mapbox_access_token(mapbox_token)
 
 def generate_map():
-    mapbox_token = environ.get('MAPBOX_TOKEN')
     df = pd.DataFrame(retrieve_data()['Sites']).reset_index(drop=True).transpose().reset_index()
     df.columns = ['Name','Address','Category','Description','lat','lon']
     fig = px.scatter_mapbox(df, 
