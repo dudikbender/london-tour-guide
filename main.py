f15e9df6dd7ebced1d32aac074d5e48042fbeeca
David Bender <bender2242@gmail.com>
2022-01-22 20:23:17 +0000

    cleaned up main

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ from dotenv import find_dotenv, load_dotenv
 env_loc = find_dotenv('.env')
 load_dotenv(env_loc)
 
-
-
 ## Streamlit App
-# Header Text
 st.title("Our Tour of London")
-
 df = pd.DataFrame(retrieve_data()['Sites']).reset_index(drop=True).transpose().reset_index()
 df.columns = ['Name','Address','Category','Description','importance','lat','lon']
 df = df[['Name','Category','Description','importance']].sort_values('importance', ascending=False)
-#df['importance'] = [ int(x) for x in df.importance ]
 st.dataframe(df)
-
 style_options = ['open-street-map', 'white-bg', 'carto-positron', 'carto-darkmatter', 'stamen- terrain', 
                  'stamen-toner', 'stamen-watercolor','basic', 'streets', 'outdoors', 'light', 'dark', 
                  'satellite', 'satellite- streets']
